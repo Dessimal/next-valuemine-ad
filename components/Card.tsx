79034be4a53f2d6fe7714e5b6ecfaebdd749f23e
Dessimal <you@example.com>
2025-04-23 13:17:05 +0100

    resolving build errors sixth iteration

--- components/Card.tsx
+++ components/Card.tsx
@@ -5,7 +5,7 @@ interface CardProps {
   content: string;
 }
 
-const Card: React.Fc<CardProps> = ({ icon, content }) => {
+const Card: React.FC<CardProps> = ({ icon, content }) => {
   return (
     <div className="dark:bg-black w-full max-w-[600px] rounded-lg p-10 sm:p-6 shadow-lg border-1 flex flex-col gap-2 items-center justify-center">
       <div className="dark:text-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
