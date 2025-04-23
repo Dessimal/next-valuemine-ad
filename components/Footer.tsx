b8464dcbeea88e539ecaa2c79ae4568e2df267d2
Dessimal <you@example.com>
2025-04-23 13:21:35 +0100

    resolving build error seveth iteration

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,10 @@
-import React from "react";
+import React, { ReactNode } from "react";
 
-const Footer = ({ children }) => {
+type FooterProps = {
+  children: ReactNode;
+};
+
+const Footer = ({ children }: FooterProps) => {
   return (
     <footer className="w-full py-20 px-10 flex flex-row items-center justify-center">
       {children}
