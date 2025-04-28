d9217d92ad25ed4bacd788dc3f7baf234ce036e6
Dessimal <you@example.com>
2025-04-28 06:22:21 +0100

    resolving build errors

--- app/page.tsx
+++ app/page.tsx
@@ -330,7 +330,7 @@ const Home = () => {
                 setModalState("form");
                 setIsOpen(true);
               }}
-              className="relative z-50 my-8 px-6 py-8 w-full max-w-[800px] shadow-lg rounded-2xl bg-orange-600 hover:bg-sky-950 hover:border-2 hover:border-white transition-color linear duration-300">
+              className="relative z-50 my-8 px-6 py-8 w-full max-w-[800px] shadow-lg rounded-3xl bg-orange-600 hover:bg-sky-950 hover:border-2 hover:border-white transition-color linear duration-300">
               <span className="text-2xl font-bold">
                 Get Free List of Our Solar Packages
               </span>
